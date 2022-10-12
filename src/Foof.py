4bebf8de649444090fc03f0449188e1707d2b7d2
Kevin Orellana <kaorellana52@gmail.com>
2022-10-12 13:24:58 -0400

    Added Foof.py
    Alex is figuring out whatever this is lol

---
+++ src/Foof.py
@@ -0,0 +1,4 @@
+class Foo:
+  attr = "A class attribute"
+
+picklestring = pickle.dumps(Foo)
--- src/player.py
+++ src/player.py
@@ -5,18 +5,20 @@ import os
 
 class Player(sprite.Sprite):
     def __init__(self):
-        super.__init__(self)
+        super().__init__()
         self.images = []
         img = pygame.image.load("..\images\dwayneson.png")
         self.images.append(img)
         self.image = self.images[0]
         self.rect = self.image.get_rect()
-        self.prefix = "PH_down_"
-
+        prefix = "PH_down_"
+        
         for i in range(6):
             if i == 3:
-                self.prefix = "PH_right_"
-            img = pygame.image.load(os.path.join(("..", "images", self.prefix + str(i) + '.png')).convert())
+                prefix = "PH_right_"
+            print("____TEST BELOW____")
+            print(prefix + str(i))
+            img = pygame.image.load(os.path.join(("../images", prefix + str(i) + '.png')).convert())
             self.images.append(img)
             self.image = self.images[0]
             self.rect = self.image.get_rect()
