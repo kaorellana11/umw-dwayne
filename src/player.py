4652d52d28758597103c09313beb8d6b9ac5b405
Kevin Orellana <kaorellana52@gmail.com>
2022-11-07 14:17:24 -0500

    Sprite animation now returns to idle position when there is no movement + added separate image array for moving upwards

--- src/player.py
+++ src/player.py
@@ -30,7 +30,9 @@ class Player(pygame.sprite.Sprite):
         sq_size *= 2
 
         self.x_images = []
-        self.y_images = []
+        self.down_images = []
+        self.up_images = []
+        self.dir = "d" #indicates last direction that the sprite moved in
 
         
         for i in range(4):
@@ -44,8 +46,14 @@ class Player(pygame.sprite.Sprite):
             self.rect = self.image.get_rect()
 
             img = pygame.image.load(os.path.join("../images", "walk", "PH_down_" + str(num) + '.png')).convert()
-            self.y_images.append(img)
-            self.image = self.y_images[0]
+            self.down_images.append(img)
+            self.image = self.down_images[0]
+            self.image = pygame.transform.scale(self.image, [sq_size, sq_size])
+            self.rect = self.image.get_rect()
+
+            img = pygame.image.load(os.path.join("../images", "walk", "PH_up_" + str(num) + ".png")).convert()
+            self.up_images.append(img)
+            self.image = self.up_images[0]
             self.image = pygame.transform.scale(self.image, [sq_size, sq_size])
             self.rect = self.image.get_rect()
 
@@ -59,35 +67,49 @@ class Player(pygame.sprite.Sprite):
     def update_pos(self):
         
         #ani decides speed of animation cycle
-        ani = 20
+        ani = 10
 
         sprites_num = len(self.x_images) - 1
 
         self.rect.x += self.move_x
         self.rect.y += self.move_y
         
+        
         if self.move_x < 0:
             self.frame += 1
             if self.frame > (sprites_num * ani):
                 self.frame = 0
             self.image = pygame.transform.flip(self.x_images[self.frame//ani], True, False)
+            self.dir = "l"
 
         if self.move_x > 0:
             self.frame += 1
             if self.frame > (sprites_num * ani):
                 self.frame = 0
             self.image = self.x_images[self.frame//ani]
+            self.dir = "r"
 
         if self.move_y < 0:
             self.frame += 1
             if self.frame > (sprites_num * ani):
                 self.frame = 0
-            self.image = pygame.transform.flip(self.y_images[self.frame//ani], False, True)
+            self.image = self.up_images[self.frame//ani]
+            self.dir = "u"
 
         if self.move_y > 0:
             self.frame += 1
             if self.frame > (sprites_num * ani):
                 self.frame = 0
-            self.image = self.y_images[self.frame//ani]
+            self.image = self.down_images[self.frame//ani]
+            self.dir = "d"
 
-        
+        if self.move_x == 0 and self.move_y == 0:
+            self.frame = 0
+            if self.dir == "l":
+                self.image = pygame.transform.flip(self.x_images[self.frame//ani], True, False)
+            if self.dir =="r":
+                self.image = self.x_images[self.frame//ani]
+            if self.dir == "u":
+                self.image = self.up_images[self.frame//ani]
+            if self.dir == "d":
+                self.image = self.down_images[self.frame//ani]        
