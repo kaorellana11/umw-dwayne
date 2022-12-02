dc2c49b2f720def163650c9225bf5376cabe8aa2
Kevin Orellana <kaorellana52@gmail.com>
2022-12-02 10:59:56 -0500

    Collision Test complete

--- collision_test.py
+++ collision_test.py
@@ -1,21 +1,52 @@
 import pygame
 
 def stop_rect():
-    global speed
-    rectangle.x += speed
+    global x_speed, y_speed, line_arr, x_swapped, y_swapped
+    collision_tolerance = 10
+    rectangle.x += x_speed
+    rectangle.y += y_speed
+
     if rectangle.right >= WIDTH or rectangle.left <= 0:
-        speed *= -1
+        x_speed *= -1
+    if rectangle.bottom >= HEIGHT or rectangle.top <= 0:
+        y_speed *= -1
+        
+    for line in line_arr:
+        if rectangle.colliderect(line):
+            print("Yeah!")
+            if (abs(rectangle.right - line.centerx) < collision_tolerance or abs(rectangle.left - line.centerx) < collision_tolerance) and x_swapped == True:
+                print("Swap X: " + str(x_swapped))
+                x_speed *= -1
+                x_swapped = False
+            if (abs(rectangle.bottom - line.centery) < collision_tolerance or abs(rectangle.top - line.centery) < collision_tolerance) and y_swapped == True:
+                print("Swap Y: " + str(y_swapped))
+                y_speed *= -1
+                y_swapped = False
+        else:
+                x_swapped = True
+                y_swapped = True
+
+
 
 running = True
 pygame.init()
 clock = pygame.time.Clock()
 
+
 WIDTH = HEIGHT = 525
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-rectangle = pygame.Rect(10, 10, 30, 30)
-line = pygame.Rect(WIDTH/2, 0, 1, HEIGHT)
-speed = 10
+rectangle = pygame.Rect(400, 400, 30, 30)
+ver_line = pygame.Rect(WIDTH/2, 0, 1, HEIGHT)
+hor_line = pygame.Rect(0, HEIGHT/2, WIDTH, 1)
+line_arr = []
+line_arr.append(ver_line)
+line_arr.append(hor_line)
+
+x_speed = 3
+y_speed = 2
+
+x_swapped = y_swapped = True
 
 while running:
     for event in pygame.event.get():
@@ -24,7 +55,9 @@ while running:
 
     screen.fill((50, 50, 50))
     pygame.draw.rect(screen, (255, 255, 255), rectangle)
-    pygame.draw.rect(screen, (255, 255, 255), line)
+    pygame.draw.rect(screen, (255, 255, 255), ver_line)
+    pygame.draw.rect(screen, (255, 255, 255), hor_line)
+    pygame.draw.circle(screen, (255, 0, 0), rectangle.center, 2) 
     stop_rect()
     pygame.display.flip()
     clock.tick(60)
